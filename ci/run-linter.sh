7ab42bfb3c3579f642cc9fad23554a4f88693fa0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-16 17:08:44 -0600

    Update dependencies

--- ci/run-linter.sh
+++ ci/run-linter.sh
@@ -8,10 +8,10 @@ echo "Code Styling with (black, flake8, isort)"
 source activate ${ENV_NAME}
 
 echo "[flake8]"
-flake8 esmlab
+flake8 esmlab_regrid
 
 echo "[black]"
-black --check --line-length=100 -S esmlab
+black --check --line-length=100 -S esmlab_regrid
 
 echo "[isort]"
-isort --recursive -w 100 --check-only esmlab
+isort --recursive -w 100 --check-only esmlab_regrid
